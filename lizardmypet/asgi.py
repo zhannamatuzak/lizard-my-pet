8a5c11436346be335f6369a456341981ff62ad0b
Zhanna Matuzak <volokhovazhanna@gmail.com>
2024-01-10 17:00:17 +0000

    Change Django project name and correct indentation issue in models.py

--- lizardmypet/asgi.py
+++ lizardmypet/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for zentrumwest project.
+ASGI config for lizardmypet project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zentrumwest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizardmypet.settings')
 
 application = get_asgi_application()

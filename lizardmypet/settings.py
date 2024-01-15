cc268ad1dcf97df69dde7725edbfe1a0d129dc65
Zhanna Matuzak <volokhovazhanna@gmail.com>
2024-01-15 12:41:04 +0000

    Change Django version to Django 3.2.16

--- lizardmypet/settings.py
+++ lizardmypet/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lizard-my-pet-59221c9c04e1.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['lizard-my-pet-59221c9c04e1.herokuapp.com']
 
 
 # Application definition

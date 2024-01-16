04e4bde24472e8544cd29e855a537410c0888d9d
Zhanna Matuzak <volokhovazhanna@gmail.com>
2024-01-16 14:06:55 +0000

    Debug wsgi.py file: wrong project name

--- lizardmypet/wsgi.py
+++ lizardmypet/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for zentrumwest project.
+WSGI config for lizardmypet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zentrumwest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizardmypet.settings')
 
 application = get_wsgi_application()

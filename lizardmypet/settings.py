cd9b1534845d338b534f0fa7deaa5cee805609ce
Zhanna Matuzak <volokhovazhanna@gmail.com>
2024-01-31 18:41:44 +0000

    Fix static root

--- lizardmypet/settings.py
+++ lizardmypet/settings.py
@@ -157,7 +157,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 #STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 MEDIA_URL = '/media/'
